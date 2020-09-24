a523c527742f817780a4765c29c0292c2da1e235
EduardGuilherme <eduguilherme48@gmail.com>
2020-09-24 02:05:28 -0300

    Adicionando os ultimos arquivos Trabalho finalizado

--- tp02XF/tp02XF/MainPage.xaml.cs
+++ tp02XF/tp02XF/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,6 +11,7 @@ namespace tp02XF
 {
     public partial class MainPage : ContentPage
     {
+        
         public MainPage()
         {
             InitializeComponent();
@@ -23,6 +25,25 @@ namespace tp02XF
         async void btnEstiloDinamico(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Page_EstiloDinamico());
+        }
+
+        async void btnEventTriggers(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page_EstiloEventTriggers());
+        }
+        
+        async void btnViewSimples(object sender, EventArgs e)
+        {
+            var contato = new Contato
+            {
+                Nome = "Eduardo Guilherme",
+                Idade = 24,
+                Profissao = "Programador Front-end",
+                Pais = "Brasil"
+            };
+            var detalhePage = new Page_viewSimples();
+            detalhePage.BindingContext = contato;
+            await Navigation.PushAsync(detalhePage);
         }
     }
 }
